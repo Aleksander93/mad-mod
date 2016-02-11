16ee72e88d3defc98c709c6fb4e8eb6d1a8293aa
unknown <anechka-sereda@mail.ru>
2016-02-11 11:40:06 +0300

    первые данные

---
+++ leson1.R
@@ -0,0 +1,24 @@
+x=1:100
+y=sin(x)
+plot(x,y)
+#numerical числовые , мат операции character- текст символ logic- логика true\false
+x=1:10
+y=c(T,F,TRUE,FALSE)
+t=1
+x+t
+x+w
+w="А"
+u=c("А",23,T)
+S=c(1,T,F,0)
+# char numeric logic
+f=c(x,y,u,s)
+# c оператор объединения векторов 
+a=2
+b=3
+a=b
+a<-2
+b<-3
+a->B
+assight("t","text")
+paste("A",a,sep="G")
+
